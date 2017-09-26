f08a5aa6d76871f260fb15ad80f858e88c15f406
Abdullah Doğan <abdullah.dogan@antasya.com>
2017-09-26 13:03:51 +0300

    projeye dosyalar eklendi

---
+++ src/main/java/com/antasya/XxXApplication.java
@@ -0,0 +1,12 @@
+package com.antasya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxXApplication.class, args);
+	}
+}
---
+++ src/main/java/com/antasya/model/Mahmud.java
@@ -0,0 +1,21 @@
+package com.antasya.model;
+
+public class Mahmud {
+	
+	private int id;
+	private int serialNumber;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(int serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	
+}
